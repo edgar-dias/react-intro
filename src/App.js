f6b917abea15cc69658dd205303fc8051cd0c93c
Edgar Dias <edgarus@gmail.com>
2020-08-28 21:17:26 +0100

    add two-way binding and component styling

--- src/App.js
+++ src/App.js
@@ -12,22 +12,46 @@ class App extends Component {
         otherState: ' some value'
     }
 
-    switchNameHandler = () => {
+    switchNameHandler = (newName) => {
         this.setState({persons: [
-            { name: 'Daniel D', age: 12},
+            { name: newName, age: 12},
             { name: 'Rafael D', age: 9},
             { name: 'Joana D', age: 6}
         ] })
     }
 
+    nameChangeHandler = (event) => {
+        this.setState({persons: [
+            { name: 'Daniel', age: 12},
+            { name: event.target.value, age: 9},
+            { name: 'Joana', age: 6}
+        ] })
+    }
+
     render() {
+        const style = {
+            backgroundColor: 'white',
+            font: 'inherit',
+            border: '1px solid blue',
+            padding: '8px',
+            cursor: 'pointer'
+        };
+
         return (
             <div className="App">
                 <h1>My react App</h1>
                 <p>Let's go deeper</p>
-                <button onClick={this.switchNameHandler}>Switch Name</button>
+                <button 
+                style={style}
+                onClick={() => this.switchNameHandler('Dani')}
+                >Switch Name</button>
                 <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-                <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>
+                <Person
+                 name={this.state.persons[1].name} 
+                 age={this.state.persons[1].age}
+                 click={this.switchNameHandler.bind(this, 'Dan!')}
+                 changed={this.nameChangeHandler}
+                 >
                     My hobbies: Swimming
                 </Person>
                 <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
